8e4086f7e0149d51af5d824c428b939b043afbbb
Your Name <you@example.com>
2025-07-03 23:58:29 +0530

    some stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,12 +13,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    
     <html lang="en">
       <body>
         {children}
       </body>
     </html>
-    </ClerkProvider>
+    
   );
 }
