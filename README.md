12856ece19523e987df49c3345fc3372ddac7888
Your Name <raghavonpc@gmail.com>
2025-07-06 14:26:14 +0530

    added prisma to the project and wrote the video model

--- README.md
+++ README.md
@@ -35,6 +35,3 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
 # crop-comp-file
-
-
-checking the green box now as my email was not added to git bash here 
---
+++ app/api/videos/route.ts
@@ -0,0 +1,24 @@
+import {NextRequest, NextResponse} from "next/server"
+
+import { PrismaClient } from "@/app/generated/prisma"
+
+//to connect with prisma 
+
+const prisma = new PrismaClient()
+
+export async function GET(req:NextRequest){
+    try {
+        const videos = await prisma.video.findMany({
+            orderBy:{createdAt:"desc"}
+        })
+        return NextResponse.json(videos)
+    } 
+    catch (error) {
+        return NextResponse.json({error:"errror while fetching videos"})
+    }
+    //best practise is to disconnect prisma 
+    finally {
+        await prisma.$disconnect()
+    }
+
+}
