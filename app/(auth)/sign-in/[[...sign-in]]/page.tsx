d2118c6b66d234cc85cdb232a7cb8146b757fedf
Your Name <raghavonpc@gmail.com>
2025-07-17 22:53:13 +0530

    some ui changes

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from '@clerk/nextjs'
-
+import { neobrutalism } from '@clerk/themes'
+// basic styling karde 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <SignIn appearance={{ baseTheme: neobrutalism }} />
+    </div>
+  )
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignUp } from '@clerk/nextjs'
-
+import { neobrutalism } from '@clerk/themes'
 export default function Page() {
-  return <SignUp />
+  return(
+  <div className='flex items-center justify-center h-screen'>
+   <SignUp appearance={{ baseTheme: neobrutalism }} />
+  </div>
+  
+)
 }
