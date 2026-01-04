64dc3bda3f82b8fa1a41ade2aa83f7e9a72f9fc2
Your Name <raghavonpc@gmail.com>
2026-01-04 16:31:09 +0530

    deployment stuff

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -1,6 +1,6 @@
 import {NextRequest, NextResponse} from "next/server"
 
-import { PrismaClient } from "@/app/generated/prisma"
+import { PrismaClient } from "../../generated/prisma";
 
 //to connect with prisma 
 
